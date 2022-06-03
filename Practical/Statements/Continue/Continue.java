5852a11583c826207b907e72d05c8cb4f3adfc8c
CrazyIndianCoder <103626070+CrazyIndianCoder@users.noreply.github.com>
2022-06-03 14:02:50 +0530

    Add files via upload

---
+++ Practical/Statements/Continue/Continue.java
@@ -0,0 +1,15 @@
+package Statements;
+//Java program to print the
+//working of continue statement
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			if(i==2) {
+			 continue;
+	}
+			System.out.println(i);
+}
+	}
+}
