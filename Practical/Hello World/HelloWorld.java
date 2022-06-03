4295d5ee62259ca457f53fb0fa9417e682387dbf
CrazyIndianCoder <103626070+CrazyIndianCoder@users.noreply.github.com>
2022-06-03 13:20:49 +0530

    Add files via upload

---
+++ Practical/Hello World/HelloWorld.java
@@ -0,0 +1,11 @@
+package corejava;
+// Java program to print Hello World
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Printing the result
+		System.out.println("helloworld");
+		}
+
+}
