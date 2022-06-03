972cc92f4b2378c97f9725ef3d25ee56ad330eb8
CrazyIndianCoder <103626070+CrazyIndianCoder@users.noreply.github.com>
2022-06-03 13:02:12 +0530

    Add files via upload

---
+++ Practical/Array/SingleDimensional/SumOfArray.java
@@ -0,0 +1,38 @@
+package Array;
+// Java program to print sum of array
+
+import java.util.*;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		
+		int len;
+		int sum=0;
+	// Creating object of scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter Array Length: ");
+		
+		len = sc.nextInt();
+		
+		int a[]= new int[len];
+		System.out.print("\nEnter: "+ len + " Elements To Store In Array\n");
+		{
+		for (int i=0; i<len;i++) {
+			a[i]=sc.nextInt();
+			
+		}
+		
+		System.out.println(" ");
+		System.out.println("Elements In Array Are: \n");
+		for (int i=0; i<len;i++) {
+			System.out.print(a[i]+ " ");
+        sum = sum + a[i] ;
+			
+		}
+		System.out.println("\n Sum of the Elements Arrays Are: "+ sum);
+		
+	}
+	}
+}
