0e2c68b48c573e1b531c629c95e1b01c8e18c286
CrazyIndianCoder <103626070+CrazyIndianCoder@users.noreply.github.com>
2022-06-07 11:12:36 +0530

    Add files via upload

---
+++ Practical/Maven Project/App.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworld;
+// Creating a simple hello world project
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
