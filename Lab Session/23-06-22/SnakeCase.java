82768335df5b1971519132772d7a812ce1958c3d
CrazyIndianCoder <103626070+CrazyIndianCoder@users.noreply.github.com>
2022-06-24 13:39:54 +0530

    Add files via upload

---
+++ Lab Session/23-06-22/SnakeCase.java
@@ -0,0 +1,38 @@
+// Java program to illustrate the working of snake case
+package lab;
+
+ class SnakeCase
+{	
+	public String camelToSnake(String str)
+	{
+	//Empty String
+	String stt="";
+		
+	//Append First Character To Lower Case
+	stt+=Character.toLowerCase(str.charAt(0));
+
+	for(int i=1;i<str.length();i++)
+	{
+	char ch=str.charAt(i);
+		 
+	//Checking The Characters Unicode Identifier or Not
+	if (Character.isUnicodeIdentifierPart(ch))
+	{
+	stt=stt+'_';
+	stt=stt+Character.toLowerCase(ch);
+	}
+	else
+	{
+	stt=stt+ch;
+	}  
+	}
+	return stt;
+	 }
+
+	public static void main(String[] args) 
+	{
+	//Creating a Object of SnakeCase
+	SnakeCase sc=new SnakeCase();
+	System.out.println(sc.camelToSnake("Onkar"));
+		}
+	}
